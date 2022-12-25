7a9c91a75cfb60debcca7353397d9f89065c0f47
Justin <theskaterjesus@gmail.com>
2022-12-25 20:02:14 +0000

    Added more colours to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'vd-magenta': 'hsl(300, 43%, 22%)',
+        'soft-pink': 'hsl(333, 80%, 67%)',
+        'vg-magenta': 'hsl(303, 10%, 53%)',
+        'lg-magenta': 'hsl(300, 24%, 96%)',
+      },
       fontFamily: {
         'league-spartan': ['League Spartan', 'sans-serif'],
       }
